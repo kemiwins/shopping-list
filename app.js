bf87809b9a6c64973ef5db26be77eeedc03e8eb9
kemiwins <kemi.mail@gmail.com>
2014-05-01 20:46:43 -0400

    Added jquery and background image

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	$(".add").on("click", function() {
+		var value=$.trim($("#items_input").val());
+		$("<li>").insertAfter("li:last").append(value);
+		if(value == "") {
+			$("li:last").hide();	
+			alert("Please enter an item!");
+		}
+		if(value > "") {
+			$("li:last").show();	
+		}
+	})
+	$(".list").on("click", "li", function() {
+		$(this).toggleClass("selected");	
+	})
+	$(".remove").mousedown(function(){
+		$(".selected").hide("li");
+	})
+	$(".clear").mousedown(function(){
+		$("li").addClass("selected").hide("li");
+		$("#items_input").val(""); 
+	})
+	$("#items_input").keydown(function(event){
+		if(event.keyCode == 13) {
+			var value=$.trim($("#items_input").val());
+			$("<li>").insertAfter("li:last").append(value);
+			if(value == "") {
+				$("li:last").hide();	
+				alert("Please enter an item!");
+			}
+			if(value > "") {
+				$("li:last").show();	
+			}
+		}
+	})
+});
